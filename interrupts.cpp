dea7832548771302b697d93234d9791b800dd228
rehmaaaa <rehmamuzammil2004@gmail.com>
2025-10-04 12:40:41 -0400

    basic simulator logic

--- interrupts.cpp
+++ interrupts.cpp
@@ -1,43 +1,71 @@
 /**
- *
  * @file interrupts.cpp
  * @author Sasisekhar Govind
  *
  */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <interrupts.hpp>
+#include <vector>
 
-#include<interrupts.hpp>
+long long current_time = 0;
+std::map<int, std::string> vector_table;
+std::map<int, int> device_table;
+std::ifstream trace_file("trace.txt");
+std::ofstream execution_file("execution.txt");
 
 int main(int argc, char** argv) {
-
-    //vectors is a C++ std::vector of strings that contain the address of the ISR
-    //delays  is a C++ std::vector of ints that contain the delays of each device
-    //the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
     std::ifstream input_file(argv[1]);
 
-    std::string trace;      //!< string to store single line of trace file
-    std::string execution;  //!< string to accumulate the execution output
+    std::string trace;
+    std::string execution;
 
-    /******************ADD YOUR VARIABLES HERE*************************/
+    // input file opened.
+    if (!input_file.is_open()) {
+        std::cerr << "Error: Unable to open trace file from argument." << std::endl;
+        return 1;
+    }
 
-
-
-    /******************************************************************/
-
-    //parse each line of the input trace file
-    while(std::getline(input_file, trace)) {
+    // parsing each line.
+    while (std::getline(input_file, trace)) {
         auto [activity, duration_intr] = parse_trace(trace);
 
-        /******************ADD YOUR SIMULATION CODE HERE*************************/
+        if (activity == "CPU") {
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU burst\n";
+            current_time += duration_intr;
+        } else if (activity == "SYSCALL") {
+            auto [log_str, updated_time] = intr_boilerplate(current_time, duration_intr, 10, vectors);
+            execution += log_str;
+            current_time = updated_time;
 
+            execution += std::to_string(current_time) + ", 40, SYSCALL: run the ISR (device driver)\n";
+            current_time += 40;
+        } else if (activity == "END_IO") {
 
-
-        /************************************************************************/
-
+            auto [log_str, updated_time] = intr_boilerplate(current_time, duration_intr, 10, vectors);
+            execution += log_str;
+            current_time = updated_time;
+            
+            
+            execution += std::to_string(current_time) + ", 40, ENDIO: run the ISR (device driver)\n";
+            current_time += 40;
+            
+        
+            execution += std::to_string(current_time) + ", 1, IRET\n";
+            current_time += 1;
+            
+            execution += std::to_string(current_time) + ", 10, context restored\n";
+            current_time += 10;
+            
+            execution += std::to_string(current_time) + ", 1, switch to user mode\n";
+            current_time += 1;
+        }
     }
 
     input_file.close();
-
     write_output(execution);
 
     return 0;
